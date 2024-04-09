2299fa7ed13a5cc7f34b98850b2e6a0b0cc0a89e
austin-aguilar <austin47a@gmail.com>
2024-04-08 21:10:43 -0400

    added contact page

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,11 +1,24 @@
 import "./contact.css";
+import { SiGithub, SiLinkedin } from 'react-icons/si';
+import { MdEmail } from 'react-icons/md';
+import { Link } from "react-router-dom";
 
 const contact = () => {
     return (
         <div className='contactContainer'>
-            <div>
-                <h3>Email:</h3>
-                
+            <div className="contactInfo">
+                <h2>Get in touch!</h2>     
+            </div>
+            <div className="contactLinks">
+                <Link to="https://github.com/austin-aguilar">
+                    <SiGithub size="32px" /> 
+                </Link>
+                <Link to="https://www.linkedin.com/in/austinaguilarmecs/">
+                    <SiLinkedin  size="32px"/> 
+                </Link>
+                <Link to="mailto:austin47a@gmail.com">
+                    <MdEmail  size="32px"/> 
+                </Link>
             </div>
         </div>
     )
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -2,6 +2,7 @@ import "./home.css";
 // import Austin from '../../assets/Austin_Photo.jpeg'
 import { SiGithub, SiLinkedin } from 'react-icons/si';
 import { Link } from "react-router-dom";
+import { MdEmail } from 'react-icons/md';
 
 const Home = () => {
 
@@ -22,6 +23,9 @@ const Home = () => {
                     </Link>
                     <Link to="https://www.linkedin.com/in/austinaguilarmecs/">
                         <SiLinkedin /> 
+                    </Link>
+                    <Link to="mailto:austin47a@gmail.com">
+                        <MdEmail/> 
                     </Link>
             </div>
         </div>
