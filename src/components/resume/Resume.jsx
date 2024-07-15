450bb29ef833352dcf5cf79d1dbbb9c32d2512ba
austin-aguilar <austin47a@gmail.com>
2024-07-15 15:09:47 -0400

    updated resume

--- src/components/resume/Resume.jsx
+++ src/components/resume/Resume.jsx
@@ -5,7 +5,7 @@ const resume = () => {
         <div className="container">
             <h1 className='header'>Resume</h1>
             <div className="resumeContainer">
-                <iframe className="resumePdf" src='src/assets/resume_spr_24pdf.pdf' width="80%" height="100%"/>
+                <iframe className="resumePdf" src='src/assets/aguilar-resume-summer24.pdf' width="80%" height="100%"/>
             </div>
         </div>
  
